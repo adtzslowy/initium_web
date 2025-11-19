7a1c6f8ed120ea0078b4b5b2a1f36b5b0d6c29b1
adtzslowy <user.aditprasetyo25@gmail.com>
2025-11-19 11:40:48 +0700

    updated

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,7 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: '/initium_web/',
   plugins: [
         react(),
         tailwindcss(),
